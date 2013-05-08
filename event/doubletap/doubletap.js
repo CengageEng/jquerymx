e5facd0563d4df6d9c09973fdb11c4ab498276e8
Nick Matthews <nick.matthews@esentire.com>
2013-05-08 18:16:53 -0400

    NG-15145 Adding support for the double touch event

---
+++ event/doubletap/doubletap.js
@@ -0,0 +1,33 @@
+/**
+ * @add jQuery.event.special
+ */
+
+steal('jquery/event/livehack').then(function($){
+    function registerTapEventHelper(touchStartEvent) {
+        $.event.setupHelper( ['dbltap'], touchStartEvent, function(ev){
+
+            var delegate = ev.delegateTarget || ev.currentTarget,
+                selector = ev.handleObj.selector,
+                entered = this;
+
+            var now = new Date().getTime();
+            var lastTouch = $(this).data('lastTouch') || now + 1 /** the first time this will make delta a negative number */;
+            var delta = now - lastTouch;
+
+            if(delta < 500 && delta > 0){
+                ev.preventDefault();
+                $.each($.event.find(delegate, ['dbltap'], selector), function(){
+                    if( !(ev.isPropagationStopped() || ev.isImmediatePropagationStopped()) ) {
+                        this.call(entered, ev, {start : lastTouch, end: now});
+                    }
+                });
+            }
+            $(this).data('lastTouch', now);
+        });
+
+    }
+
+    if ('ontouchend' in document) {
+        registerTapEventHelper('touchend');
+    }
+});
